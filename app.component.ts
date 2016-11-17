5ad79c5fe043b701dc12f00daf4be2ab7ec8fa99
felipe-mesquita <felimc@gmail.com>
2016-11-17 17:21:53 -0300

    new files and tests

---
+++ app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  	<h1>Inline Template</h1>
+  	<fa-other></fa-other>
+  	<fa-another></fa-another>
+  `,
+  styles: [`
+  	h1{
+  		color: red;
+  	}
+  `]
+})
+export class AppComponent {
+  title = 'I changed it 2!';
+}
